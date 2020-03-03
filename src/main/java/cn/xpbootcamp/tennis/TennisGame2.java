b688349cba88e96dc42d84cd512f931622c3c0b2
zhoupeng <abc1095014900@qq.com>
2020-03-03 15:20:42 +0800

    优化if

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -15,7 +15,7 @@ public class TennisGame2 implements TennisGame {
         String score = "";
         if (p1Point == p2Point) {
             if (p1Point < 3) {
-                score = scores[p1Point].getValue() + "-All";
+                score = scores[p1Point].getValue() + ALL_PREF;
             } else {
                 score = Score.DEUCE.getValue();
             }
@@ -24,55 +24,35 @@ public class TennisGame2 implements TennisGame {
         if (p1Point > 0 && p2Point == 0 || p2Point > 0 && p1Point == 0) {
             p1Res = scores[p1Point].getValue();
             p2Res = scores[p2Point].getValue();
-            score = p1Res + "-" + p2Res;
+            score = p1Res + PREFIX + p2Res;
 
         }
 
-        if (p1Point > p2Point)
-            if (p1Point < 4) {
-                if (p1Point == 2)
-                    p1Res = "Thirty";
-                if (p1Point == 3)
-                    p1Res = "Forty";
-                if (p2Point == 1)
-                    p2Res = "Fifteen";
-                if (p2Point == 2)
-                    p2Res = "Thirty";
-                score = p1Res + "-" + p2Res;
+        if (p1Point > p2Point || p2Point > p1Point) {
+            if (p1Point < 4 || p2Point < 4) {
+                p1Res = scores[p1Point].getValue();
+                p2Res = scores[p2Point].getValue();
+                score = p1Res + PREFIX + p2Res;
             }
-        if (p2Point > p1Point)
-            if (p2Point < 4) {
-                if (p2Point == 2)
-                    p2Res = "Thirty";
-                if (p2Point == 3)
-                    p2Res = "Forty";
-                if (p1Point == 1)
-                    p1Res = "Fifteen";
-                if (p1Point == 2)
-                    p1Res = "Thirty";
-                score = p1Res + "-" + p2Res;
+            if (p1Point > p2Point && p2Point >= 3) {
+                score = Score.ADVANTAGE.getValue() + SPACE + PLAYER1;
             }
 
-        if (p1Point > p2Point)
-            if (p2Point >= 3) {
-                score = "Advantage player1";
+            if (p2Point > p1Point && p1Point >= 3) {
+                score = Score.ADVANTAGE.getValue() + SPACE + PLAYER2;
             }
 
-        if (p2Point > p1Point)
-            if (p1Point >= 3) {
-                score = "Advantage player2";
-            }
+        }
+
 
         if (p1Point >= 4)
-            if (p2Point >= 0)
-                if ((p1Point - p2Point) >= 2) {
-                    score = "Win for player1";
-                }
+            if ((p1Point - p2Point) >= 2) {
+                score = "Win for player1";
+            }
         if (p2Point >= 4)
-            if (p1Point >= 0)
-                if ((p2Point - p1Point) >= 2) {
-                    score = "Win for player2";
-                }
+            if ((p2Point - p1Point) >= 2) {
+                score = "Win for player2";
+            }
         return score;
     }
 
