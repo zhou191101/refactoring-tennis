d655c165883893da4bfb20677cd7c8b200d6c344
zhoupeng <abc1095014900@qq.com>
2020-03-03 15:30:19 +0800

    统一命名

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -3,8 +3,8 @@ package cn.xpbootcamp.tennis;
 import static cn.xpbootcamp.tennis.Constant.*;
 
 public class TennisGame2 implements TennisGame {
-    private int p1Point = 0;
-    private int p2Point = 0;
+    private int mScore1 = 0;
+    private int mScore2 = 0;
 
 
     private String p1Res = "";
@@ -13,43 +13,43 @@ public class TennisGame2 implements TennisGame {
     public String getScore() {
         Score[] scores = Score.values();
         String score = "";
-        if (p1Point == p2Point) {
-            if (p1Point < 3) {
-                score = scores[p1Point].getValue() + ALL_PREF;
+        if (mScore1 == mScore2) {
+            if (mScore1 < 3) {
+                score = scores[mScore1].getValue() + ALL_PREF;
             } else {
                 score = Score.DEUCE.getValue();
             }
         }
 
-        if (p1Point > 0 && p2Point == 0 || p2Point > 0 && p1Point == 0) {
-            p1Res = scores[p1Point].getValue();
-            p2Res = scores[p2Point].getValue();
+        if (mScore1 > 0 && mScore2 == 0 || mScore2 > 0 && mScore1 == 0) {
+            p1Res = scores[mScore1].getValue();
+            p2Res = scores[mScore2].getValue();
             score = p1Res + PREFIX + p2Res;
 
         }
 
-        if (p1Point > p2Point || p2Point > p1Point) {
-            if (p1Point < 4 || p2Point < 4) {
-                p1Res = scores[p1Point].getValue();
-                p2Res = scores[p2Point].getValue();
+        if (mScore1 > mScore2 || mScore2 > mScore1) {
+            if (mScore1 < 4 || mScore2 < 4) {
+                p1Res = scores[mScore1].getValue();
+                p2Res = scores[mScore2].getValue();
                 score = p1Res + PREFIX + p2Res;
             }
-            if (p1Point > p2Point && p2Point >= 3) {
+            if (mScore1 > mScore2 && mScore2 >= 3) {
                 score = Score.ADVANTAGE.getValue() + SPACE + PLAYER1;
             }
 
-            if (p2Point > p1Point && p1Point >= 3) {
+            if (mScore2 > mScore1 && mScore1 >= 3) {
                 score = Score.ADVANTAGE.getValue() + SPACE + PLAYER2;
             }
 
         }
 
 
-        if (p1Point >= 4)
+        if (mScore1 >= 4)
             if (isP1MoreThanP2Two()) {
                 score = WIN + PLAYER1;
             }
-        if (p2Point >= 4)
+        if (mScore2 >= 4)
             if (isP2MoreThanP1Two()) {
                 score = WIN + PLAYER2;
             }
@@ -57,19 +57,19 @@ public class TennisGame2 implements TennisGame {
     }
 
     private boolean isP2MoreThanP1Two() {
-        return (p2Point - p1Point) >= 2;
+        return (mScore2 - mScore1) >= 2;
     }
 
     private boolean isP1MoreThanP2Two() {
-        return (p1Point - p2Point) >= 2;
+        return (mScore1 - mScore2) >= 2;
     }
 
     private void P1Score() {
-        p1Point++;
+        mScore1++;
     }
 
     private void P2Score() {
-        p2Point++;
+        mScore2++;
     }
 
     public void wonPoint(String player) {
--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -4,29 +4,29 @@ import static cn.xpbootcamp.tennis.Constant.*;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
+    private int mScore1;
+    private int mScore2;
 
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (mScore2 < 4 && mScore1 < 4 && !(mScore2 + mScore1 == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[mScore2];
+            return (mScore2 == mScore1) ? s + "-All" : s + "-" + p[mScore1];
         } else {
-            if (p1 == p2)
+            if (mScore2 == mScore1)
                 return "Deuce";
-            s = p1 > p2 ? PLAYER1 : PLAYER2;
-            return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = mScore2 > mScore1 ? PLAYER1 : PLAYER2;
+            return ((mScore2 - mScore1) * (mScore2 - mScore1) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName.equals(PLAYER1))
-            this.p1 += 1;
+            this.mScore2 += 1;
         else
-            this.p2 += 1;
+            this.mScore1 += 1;
 
     }
 
