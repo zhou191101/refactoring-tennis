487944c4cf213b27d2acf075422cc6270a4b82bb
zhoupeng <abc1095014900@qq.com>
2020-03-03 15:46:18 +0800

    判断语句提取

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -9,7 +9,7 @@ public class TennisGame3 implements TennisGame {
 
 
     public String getScore() {
-        if (p2Score < 4 && p1Score < 4 && !(p2Score + p1Score == 6)) {
+        if (p2Score < 4 && p1Score < 4 && !isP1P2EqualsSix()) {
             Score[] scores = Score.values();
             String value = scores[p2Score].getValue();
             return (p2Score == p1Score) ? value + ALL_PREF : value + PREFIX + scores[p1Score].getValue();
@@ -17,10 +17,18 @@ public class TennisGame3 implements TennisGame {
             if (p2Score == p1Score)
                 return Score.DEUCE.getValue();
             String player = p2Score > p1Score ? PLAYER1 : PLAYER2;
-            return ((p2Score - p1Score) * (p2Score - p1Score) == 1) ? Score.ADVANTAGE.getValue() + SPACE
+            return isP2P1DiffOne() ? Score.ADVANTAGE.getValue() + SPACE
                     + player : WIN + player;
 
         }
+    }
+
+    private boolean isP1P2EqualsSix() {
+        return p2Score + p1Score == 6;
+    }
+
+    private boolean isP2P1DiffOne() {
+        return (p2Score - p1Score) * (p2Score - p1Score) == 1;
     }
 
     public void wonPoint(String playerName) {
