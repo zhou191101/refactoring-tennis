c4256e44d21e986fb6ffc9c7b3fbe53095ff8129
zhoupeng <abc1095014900@qq.com>
2020-03-03 14:52:50 +0800

    枚举替换

--- src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame2.java
@@ -11,40 +11,26 @@ public class TennisGame2 implements TennisGame {
     public String p2Res = "";
 
     public String getScore() {
+        Score[] scores = Score.values();
         String score = "";
         if (p1Point == p2Point) {
-            Score[] scores = Score.values();
-            if (p1Point < 4) {
+            if (p1Point < 3) {
                 score = scores[p1Point].getValue() + "-All";
-            }
-            if (p1Point >= 3) {
+            } else {
                 score = Score.DEUCE.getValue();
             }
         }
 
-
-        if (p1Point > 0)
+        if (p1Point > 0 )
             if (p2Point == 0) {
-                if (p1Point == 1)
-                    p1Res = "Fifteen";
-                if (p1Point == 2)
-                    p1Res = "Thirty";
-                if (p1Point == 3)
-                    p1Res = "Forty";
-
-                p2Res = "Love";
+                p1Res = scores[p1Point].getValue();
+                p2Res = scores[p2Point].getValue();
                 score = p1Res + "-" + p2Res;
             }
         if (p2Point > 0)
             if (p1Point == 0) {
-                if (p2Point == 1)
-                    p2Res = "Fifteen";
-                if (p2Point == 2)
-                    p2Res = "Thirty";
-                if (p2Point == 3)
-                    p2Res = "Forty";
-
-                p1Res = "Love";
+                p1Res = scores[p1Point].getValue();
+                p2Res = scores[p2Point].getValue();
                 score = p1Res + "-" + p2Res;
             }
 
